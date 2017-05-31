66e84f1b7cfab03ea8b3dc6cb5fde4d28abd67c3
Marcio Silva <marcioadsworks@gmail.com>
2017-05-31 17:03:34 -0300

    assets e inicio do texto

---
+++ Assets/TextController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+	public Text texto;
+
+	// Use this for initialization
+	void Start () {
+		texto.text = "Bem vindo ao Prison World!\n" +
+		" Aperte Espaço para Iniciar.";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			texto.text = "Você esta em uma cela da Prisão, e quer escapar." +
+				"você vê alguns lençois sujos na cama, um espelho na parede e uma porta," +
+				" ela parece estar fechada pelo lado de fora.\n\n" +
+				"Aperte L para verificar os Lençois\n" +
+				"Aperte E para verificar o Espelho\n" +
+				"Aperte P para verificar a Porta";
+		}
+	}
+}
